e1c10ff398f76b7c1b961cff9c9227e0f52c06ad
dyh <1358366+dyh@users.noreply.github.com>
2024-05-28 11:41:31 +0800

    update to support ubuntu22.04，win10，macos.

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # bing_wallpaper
+
+> python 3.8以上，支持 ubuntu22.04，win10，macos（理论上，尚未测试）。 
+
+1. https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1
+
+```
+{
+    "images":
+    [
+        {
+            "startdate": "20240526",
+            "fullstartdate": "202405261600",
+            "enddate": "20240527",
+            "url": "/th?id=OHR.SestriLevante_ZH-CN9286254645_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+            "urlbase": "/th?id=OHR.SestriLevante_ZH-CN9286254645",
+            "copyright": "塞斯特里莱万特的沉默湾，利古里亚大区，意大利 (© StevanZZ/Getty Images)",
+            "copyrightlink": "https://www.bing.com/search?q=%E5%A1%9E%E6%96%AF%E7%89%B9%E9%87%8C%E8%8E%B1%E4%B8%87%E7%89%B9&form=hpcapt&mkt=zh-cn",
+            "title": "我们去乘船吧！",
+            "quiz": "/search?q=Bing+homepage+quiz&filters=WQOskey:%22HPQuiz_20240526_SestriLevante%22&FORM=HPQUIZ",
+            "wp": true,
+            "hsh": "fe54aa78c01caf275decb29977f538e7",
+            "drk": 1,
+            "top": 1,
+            "bot": 1,
+            "hs":
+            []
+        }
+    ],
+    "tooltips":
+    {
+        "loading": "正在加载...",
+        "previous": "上一个图像",
+        "next": "下一个图像",
+        "walle": "此图片不能下载用作壁纸。",
+        "walls": "下载今日美图。仅限用作桌面壁纸。"
+    }
+}
+```
+
+```
+"urlbase": "/th?id=OHR.SestriLevante_ZH-CN9286254645"
+```
+
+
+2. https://www.bing.com/th?id=OHR.SestriLevante_ZH-CN9286254645_UHD.jpg
---
+++ main.py
@@ -0,0 +1,100 @@
+import ctypes
+import os
+import json
+import platform
+import requests
+
+
+def get_os():
+    """
+    获取操作系统
+    :return:
+    """
+    system_text = platform.system()
+
+    # 判断操作系统
+    if system_text == 'Windows':
+        return 'windows'
+    elif system_text == 'Linux':
+        output = os.uname().version.lower()
+        if 'ubuntu' in output:
+            return 'ubuntu'
+        else:
+            return 'linux'
+    elif system_text == 'Darwin':
+        return 'macos'
+    else:
+        return 'unknown'
+    pass
+
+
+if __name__ == "__main__":
+
+    # 获取程序运行绝对路径
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    # 拼接图片绝对路径
+    local_img_file_path = os.path.join(current_path, 'background.jpg')
+
+    # bing api
+    bing_api_url = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&'
+
+    response = requests.get(bing_api_url)
+    # bytes -> string
+    json_text = response.content.decode('utf-8')
+    # string -> list[]
+    list_json = json.loads(json_text)
+    # 获取图片文件名称 urlbase
+    # "urlbase": "/th?id=OHR.SestriLevante_ZH-CN9286254645"
+    img_path = list_json['images'][0]['urlbase']
+
+    # 拼接图片完整 url
+    # https://www.bing.com/th?id=OHR.SestriLevante_ZH-CN9286254645_UHD.jpg
+    img_url = 'https://www.bing.com' + img_path + '_UHD.jpg'
+    response = requests.get(img_url)
+
+    img_data = response.content
+
+    try:
+        # 保存图片到本地
+        with open(local_img_file_path, 'wb') as img:
+            img.write(img_data)
+
+        system = get_os()
+
+        notify = 'notify-send "done"'
+
+        if system == 'ubuntu':
+            # 亮色主题
+            set_command = "gsettings set org.gnome.desktop.background picture-uri file://{}".format(
+                local_img_file_path)
+            os.system(set_command)
+
+            # 深色主题
+            set_command = "gsettings set org.gnome.desktop.background picture-uri-dark file://{}".format(
+                local_img_file_path)
+            os.system(set_command)
+
+        elif system == 'windows':
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, local_img_file_path, 3)
+        elif system == 'macos':
+            import subprocess
+
+            # 构建 AppleScript 命令
+            applescript_command = f'''
+            tell application "System Events"
+                tell every desktop
+                    set picture to "{local_img_file_path}"
+                end tell
+            end tell
+            '''
+            # 执行 AppleScript 命令
+            subprocess.run(['osascript', '-e', applescript_command])
+        elif system == 'unknown':
+            notify = 'notify-send "unknown OS, failed"'
+        pass
+
+        os.system(notify)
+
+    except Exception as e:
+        notify = 'notify-send "{}"'.format(str(e))
+        os.system(notify)
